075b667828f5797670173bf26ad0315c8d7779d6
adearch374 <adewaleadeolu93@gmail.com>
2026-08-25 03:51:07 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def inject_datetime():
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False)  # 'admin', 'teacher', 'student'
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     is_active = db.Column(db.Boolean, default=True)
